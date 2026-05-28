105d672e506e9faf987671f50e733f3fe7d9cdd7
WilliamSalcedo <wsl_1994@hotmail.com>
2026-05-28 18:32:38 -0500

    feat: implement ui-table component with loading, empty and error states

--- projects/demo-app/src/app/app.ts
+++ projects/demo-app/src/app/app.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-import { ButtonComponent, CardComponent } from 'ui-lib';
+import { ButtonComponent, CardComponent, TableComponent } from 'ui-lib';
+import { TableColumn } from 'ui-lib';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonComponent, CardComponent],
+  imports: [ButtonComponent, CardComponent, TableComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  columns: TableColumn[] = [
+    { key: 'name', header: 'Name' },
+    { key: 'species', header: 'Species' },
+    { key: 'status', header: 'Status' },
+  ];
+
+  rows = [
+    { name: 'Rick Sanchez', species: 'Human', status: 'Alive' },
+    { name: 'Morty Smith', species: 'Human', status: 'Alive' },
+    { name: 'Alien Googah', species: 'Alien', status: 'Dead' },
+  ];
+}
