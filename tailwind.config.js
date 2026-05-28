20fafc4e011dc73270c376b43aeafc4dc9f92288
WilliamSalcedo <wsl_1994@hotmail.com>
2026-05-28 10:06:09 -0500

    chore: configure content paths and fix postcss config for tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./projects/demo-app/src/**/*.{html,ts}",
+    "./projects/ui-lib/src/**/*.{html,ts}"
+  ],
   theme: {
     extend: {},
   },
