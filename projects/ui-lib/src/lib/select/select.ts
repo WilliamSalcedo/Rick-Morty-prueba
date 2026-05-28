7ef2205109f1940b660f43ca812ee30cc70653a8
WilliamSalcedo <wsl_1994@hotmail.com>
2026-05-28 09:44:26 -0500

    feat: scaffold ui-lib components (button, card, select, table)

---
+++ projects/ui-lib/src/lib/select/select.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-select',
+  imports: [],
+  templateUrl: './select.html',
+  styleUrl: './select.css',
+})
+export class Select {}
---
+++ projects/ui-lib/src/lib/table/table.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-table',
+  imports: [],
+  templateUrl: './table.html',
+  styleUrl: './table.css',
+})
+export class Table {}
